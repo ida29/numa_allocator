61a8ff473cfd3cfc7b7a728dadd6b33b041d6191
ida yuto <ida_yuto@artiza.co.jp>
2020-10-20 10:59:18 +0900

    Command "make build" was success.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 all: build clean
 
 build:
-	g++ -std=c++11 -lnuma -I./ main.cpp
+	g++ -std=c++11 -I./ main.cpp -lnuma
 
 clean:
 	rm -rf a.out
--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <numa_allocator.hpp>
 
 int main() {
-    std::vector<int, NumaAllocator<int>> v1(10, 0, NumaAllocator<int>(0));
-    std::vector<int, NumaAllocator<int>> v2(10, 0, NumaAllocator<int>());
+    std::vector<int, numa::alloc::NumaAllocator<int>> v1(10, 0, numa::alloc::NumaAllocator<int>(0));
+    std::vector<int, numa::alloc::NumaAllocator<int>> v2(10, 0, numa::alloc::NumaAllocator<int>());
 
     return 0;
 }
